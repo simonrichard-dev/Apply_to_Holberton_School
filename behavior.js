162c95ddd094cafd8cb26901e79460963161329b
Simon <simon.insa@gmail.com>
2022-02-23 12:13:26 +0100

    second commit 23.02.2022

---
+++ behavior.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+alert('Hello!');
+
+var thumbnailElement = document.getElementById("smart_thumbnail");
+
+thumbnailElement.addEventListener("click", function() {
+    alert("I saw you click!");
+    thumbnailElement.className = "";
+
+    })
+
+    
+
+
+
+}); ///do not erase this line
+
