7d7590bf4fba7b9fc81a39f8ec7b3198e6756a2c
Simon <simon.insa@gmail.com>
2022-02-23 18:50:59 +0100

    thrid commit - 23.02.2022

--- behavior.js
+++ behavior.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-alert('Hello!');
+//alert('Hello!');
 
 var thumbnailElement = document.getElementById("smart_thumbnail");
 
 thumbnailElement.addEventListener("click", function() {
-    alert("I saw you click!");
-    thumbnailElement.className = "";
+    //alert("I saw you click!");
+    if (thumbnailElement.className == "small"){
+        thumbnailElement.className = ""
+    } 
+    else {thumbnailElement.className = "small"
+    }
 
     })
-
-    
 
 
 
